e8768eaaff502b47b94f7394d7e0c3e82e65fb5d
woodmawa <will.woodman@btinternet.com>
2018-06-28 22:50:46 +0100

    updates

--- src/main/groovy/EventBus/Implementation/EventBus.groovy
+++ src/main/groovy/EventBus/Implementation/EventBus.groovy
@@ -1,77 +1,170 @@
 package EventBus.Implementation
 
+import groovy.util.logging.Log
+import groovy.util.logging.Log4j
+import groovy.util.logging.Log4j2
+import groovy.util.logging.Slf4j
+import groovyx.gpars.GParsPool
+import groovyx.gpars.activeobject.ActiveMethod
+import groovyx.gpars.activeobject.ActiveObject
 import groovyx.gpars.agent.Agent
 import groovyx.gpars.dataflow.DataflowQueue
 import groovyx.gpars.dataflow.Promise
 
+import java.util.concurrent.atomic.AtomicBoolean
 import java.util.concurrent.atomic.AtomicLong
+import java.util.stream.Collectors
+import java.util.stream.*
 
 import static groovyx.gpars.dataflow.Dataflow.task
 
 import java.util.concurrent.ConcurrentHashMap
 import java.util.concurrent.ConcurrentLinkedQueue
 
+enum EventBusProcessor {
+    START,
+    STOP
+}
+
+enum EventBusStatus {
+    STARTING,
+    RUNNING,
+    STOPPING,
+    STOPPED
+}
+
+@Slf4j
+@ActiveObject
 class EventBus {
 
+    /**
+     * index is apth string to subscribe on , value is array of subscribed listeners
+     * each expected to have an onMessage method that can be called
+     */
     ConcurrentHashMap messagePathSubscribers = new ConcurrentHashMap()
-
     final DataflowQueue inMessageQueue = new DataflowQueue ()
+
+    Agent status = new Agent (EventBusStatus.STOPPED)
+
     Agent eventId = new Agent (0L)
+    final done = new AtomicBoolean()  //defaults to false
 
     Promise eventProcessor
 
-    void notifyMessage (String sMessageType, def message) {
-        /*def num = eventId.val
-        List m = [num,[sMessageType,message] ]
-        inMessageQueue << m*/
+    void notifyMessage (String topic, def message) {
+        //do as async task
         task {
             eventId.send  {updateValue it + 1}  //increment message counter
-            //def num = eventId.val
-            //def m = [num,[sMessageType,message] ]
-            inMessageQueue << [eventId.val,[sMessageType,message] ]
+            //add message to queue with unique id and array of topic and message
+            inMessageQueue << [eventId.val, [topic,message] ]
         }
     }
 
-    void addSubscriber (String sMessagePath, def subscriberInstance ) {
-        def subscribers = messagePathSubscribers.get(sMessagePath)
+    void addSubscriber (String topic, def subscriberInstance ) {
+        //see if path string already registered as key
+        def subscribers = messagePathSubscribers.get(topic)
         if (!subscribers) {
-            def subscriberList = [subscriberInstance]
-            messagePathSubscribers.putIfAbsent(sMessagePath, subscriberList)
+            messagePathSubscribers.putIfAbsent(topic, [subscriberInstance])
         }
         else {
-            subscribers << subscriberInstance
-            messagePathSubscribers.put(subscribers)
+            if (!subscribers.contains(subscriberInstance)) {
+                subscribers << subscriberInstance
+            }
+
+             messagePathSubscribers.put(topic, subscribers)
         }
 
     }
 
-    //hack manual step through
-    void processMessage () {
-        Map event = inMessageQueue.val
-        def messagePath = event[1][0]
-        def message = event[1][1]
+    void removeSubscriber (String topic, def subscriberInstance) {
+        def subscribers = messagePathSubscribers.get(topic)
+        if (subscribers) {
+            List reducedSubscribersList = subscribers.stream().filter {!(it.is(subscriberInstance))}.collect(Collectors.toList());
 
-        def subscriberList = messagePathSubscribers[messagePath]
-        subscriberList.each {it.onMessage (messagePath, message)}
+            //is same list or another - do i n3eed to resave to map?
+            messagePathSubscribers.put(topic, reducedSubscribersList)
+        }
+    }
+
+    void clearAllSubscribers (String topic) {
+        messagePathSubscribers.put(topic, [])
+    }
+
+
+    //private method to deliver the message to subscribers
+    //@ActiveMethod
+    private void  processMessage (String topic, message) {
+        assert topic
+        def subscriberList = messagePathSubscribers[topic]
+
+        log.debug "processing message for topic $topic "
+        /*subscriberList.each {
+            boolean ans = it.respondsTo("onMessage")
+            if (it.respondsTo("onMessage"))
+                it.onMessage (topic, message)} */
+        Closure dispatchMessage = {subscriber ->
+            boolean ans = subscriber.respondsTo("onMessage")
+            if (subscriber.respondsTo("onMessage" )) {
+                subscriber.onMessage(topic, message)
+            }  else {
+                log.debug "processMessage: no matching onMessage method found on subscriber"
+
+            }
+        }
+        //parallel execution to dispatch message to all subscribers in the list
+        GParsPool.withPool {
+            subscriberList.eachParallel { dispatchMessage(it) }
+        }
+        //doesnt seem to work ! Stream.of(subscriberList).parallel().forEach ({dispatchMessage(it)})
 
     }
 
     void start() {
-        /*
+        //start a task - but it will stop if message is "STOP"
+        status.send {updateValue it =  EventBusStatus.STARTING}
         eventProcessor = task {
-            while (true) {
-                Map event = inMessageQueue.val
-                def messagePath = event[1][0]
+
+            log.debug "start: event bus processor started, status : $status.val "
+            status.send {updateValue it =  EventBusStatus.RUNNING}
+
+            while (!done.get()) {
+                List event = inMessageQueue.val
+                def eventNum = event [0]
+                def topic = event[1][0]
                 def message = event[1][1]
 
-                def subscriberList = messagePathSubscribers[messagePath]
-                subscriberList.each {it.onMessage (messagePath, message)}
-
+                if (message.class == EventBusProcessor) {
+                    if (message == EventBusProcessor.STOP) {
+                        status.send {updateValue it =  EventBusStatus.STOPPING}
+                        done.set(true)
+                        break
+                    }
+                }
+                processMessage (topic, message)
             }
-        }*/
+            status.send {updateValue it =  EventBusStatus.STOPPED}
+            log.debug "start: event bus processor now stopped, state : $status.val , done flag is : ${done.get()}"
+            return status.val
+        }
     }
 
     void stop() {
-        //eventProcessor.
+        //will step even if there messages left in queue to process
+        //seems to need this long for start task to kick in and start processing
+        // if go earlier done flag gets set before a chance to read the queue - so delay added
+        sleep (500)
+        def t = task {
+            done.set(true)
+            status.send {updateValue it =  EventBusStatus.STOPPING}
+            log.debug "stopTask completed,  returning status : $status.val"
+            status.val
+        }
+
+        //wait till task stops
+        //t.then {println "stop method main thread: set done flag to : ${done.get()} status: $it"}
+        //eventProcessor.then {println "eventProcessor task: stopped processing queue, status : $it"}
+        [t,eventProcessor]*.join()
+        log.debug "stop method main thread:: stopping the processor, remaining messages in queue ${inMessageQueue.length()} "
+
     }
 }
--- src/main/groovy/EventBus/Implementation/TestEventBusScript.groovy
+++ src/main/groovy/EventBus/Implementation/TestEventBusScript.groovy
@@ -1,8 +1,14 @@
 package EventBus.Implementation
 
 class Subscriber {
-    void onMessage (String path, def message) {
-        println "received message : $message"
+    //invoked by dynamic dispatch
+    void onMessage (String topic, message) {
+        println "received message as def  : $message"
+    }
+
+    def onMessage (String topic, String message) {
+        println "received message as String : $message"
+
     }
 }
 
@@ -14,8 +20,7 @@ eb.addSubscriber("any.event", subs)
 
 eb.start()
 eb.notifyMessage "any.event", "hellooo"
-
-eb.processMessage()
+//eb.notifyMessage "any.event", EventBusProcessor.STOP
 
 
 eb.stop()
